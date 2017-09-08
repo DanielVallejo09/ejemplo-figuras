db693280979be327b7eb4d8452fa6b06e84c4c49
daniel vallejo <daniel_vallejoquiin@hotmail.com>
2017-09-08 10:37:58 -0500

    primero push

---
+++ Figura.java
@@ -0,0 +1,36 @@
+/**
+ * Esta clase es una super clase para representar las caracterisricas
+ * comunes de muchas figuras
+ * 
+ * @autor Daniel Vallejo
+ * 
+ * 
+ */
+
+public class Figura
+{
+    protected float area;
+    
+    /**
+     * constructor de la clase figura
+     */
+    public Figura()
+    {
+        area = 0;
+    }
+    
+    /**
+     * calcula el area de la figuta
+     */
+    
+    public void calculaArea()
+    {
+         System.out.println("No se de que figura se trata");
+    }
+    
+    public void imprimirArea()
+    {
+        System.out.println("El area de la figura es:" + area);
+    }
+
+}
